204c7f227441e064f61ce9040c918fa8c79c1f7e
hartleyn-osu <hartleyn@oregonstate.edu>
2016-06-26 23:15:58 -0400

    Delete SC-script.js

--- SC-script.js
+++
@@ -1,7 +0,0 @@
-var playFirst = function() {
-	SC.stream('/partyomo/come-and-see-me-ft-drake').then(function(player){
-		player.play();
-	});
-};
-
-document.body.children[0].addEventListener("click", playFirst); 
